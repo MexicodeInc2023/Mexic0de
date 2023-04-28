c06b90e973ba264a7ab12fcdcfb5ee078a94f427
JoseArjona <j.angel.matheis@gmail.com>
2023-04-28 14:47:15 -0600

    fix scroll infinito

--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -12,5 +12,16 @@ window.addEventListener("scroll", () => {
     mountain.style.marginTop = value * 1.25 + "px";
     hill.style.marginRight = value * 0.15 + "px";
     hill.style.marginTop = value * 0.5 + "px";
+
+    /* Ajustamos el margin para evitar el scroll infinito */
+    if (value > 1000) {
+        mex.style.marginTop = "2500px";
+        agave.style.marginRight = "1500px";
+        piramid.style.marginTop = "1500px";
+        mountain.style.marginTop = "1250px";
+        hill.style.marginRight = "150px";
+        hill.style.marginTop = "500px";
+    }
+
 });
 
